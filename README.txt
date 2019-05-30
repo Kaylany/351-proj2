2e6e39cfa2d2ca0ffb73838303ce0389285fedfe
Kaylany <37362662+Kaylany@users.noreply.github.com>
2019-05-30 10:30:27 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,28 @@
+(1)
+MemberName		  section number		  Email
+
+1.Kayla Nguyen		  	03 13681		  knguyen1170@csu.fullerton.edu
+
+2.Mina Moslehpour 		03 13681	          mmoslehpour@csu.fullerton.edu
+
+3.Yi Wei Lee			03 13681		  david83031726@csu.fullerton.edu
+
+
+
+(2)The programming language we used
+C++
+
+(3)How to execute program
+1. Open the terminal and move to place where you put program's file
+2. Type g++ -o main main.cpp
+3. Type ./main
+4. Enter the memory size, page size
+
+(4)
+We communicated through Facebook message and designated different parts in this assignment, so when the final was coming
+we could still keep working on our own. Throughout the design,we were able to effectively discuss our planes and thoughts
+about the methods of implementation.
+
+Kayla Nguyen: Code, wrote the design.
+Mina Moslehpour: Code.
+Yi Wei Lee: Code, README file.
--- main.cpp
+++ main.cpp
@@ -6,204 +6,312 @@
 
 using namespace std;
 
-
-
 struct process {
-	int number;
-	int starttime;
-	int finishtime;
-	int numberofBlocks;
-	vector<int> blockofsize;
+    int number;
+    int starttime;
+    int finishtime;
+    int numberofBlocks;
+    int turnAroundTime;
+    vector<int> blockofsize;
 };
 struct memoryblock {
-	int start;
-	int end;
-	int processnumber;
-	int pagenumber;
-	bool free;
+    int start;
+    int end;
+    int processnumber;
+    int pagenumber;
+    bool free;
 };
 
 
 void readfile(string file, vector<process>& processlist)
 {
-	int numberOfprocess;
-	process temp;
-	ifstream filein;
-	filein.open("in1.txt");
-	if (filein.fail())
-	{
-		cout << "Can't open the file";
-		
-	}
-	filein >> numberOfprocess;
-	for (int i = 0; i < numberOfprocess; i++)
-	{
-		filein >> temp.number;
-		filein >> temp.starttime;
-		filein >> temp.finishtime;
-		filein >> temp.numberofBlocks;
-		vector<int> tempblock;
-		int block;
+    int numberOfprocess;
+    process temp;
+    ifstream filein;
+    filein.open("in1.txt");
+    if (filein.fail())
+    {
+        cout << "Can't open the file";
+        exit(1);
+    }
+    filein >> numberOfprocess;
+    for (int i = 0; i < numberOfprocess; i++)
+    {
+        filein >> temp.number;
+        filein >> temp.starttime;
+        filein >> temp.finishtime;
+        filein >> temp.numberofBlocks;
+        vector<int> tempblock;
+        int block;
 
-		for (int j = 0; j < temp.numberofBlocks; j++)
-		{
-			filein >> block;
-			tempblock.push_back(block);
-		}
+        for (int j = 0; j < temp.numberofBlocks; j++)
+        {
+            filein >> block;
+            tempblock.push_back(block);
+        }
 
-		temp.blockofsize = tempblock;
-		processlist.push_back(temp);
-	}
-	filein.close();
+        temp.blockofsize = tempblock;
+        processlist.push_back(temp);
+    }
+    filein.close();
 }
 
 //calc the time
 void processtime(vector<process> &processlist, vector<int> &time)
 {
-	bool temp1 =false, temp2 = false;
+    bool temp1 =false, temp2 = false;
 
-	for (int i = 0; i < processlist.size(); i++)
-	{
-		//temp1 = false;
-		//temp2 = false;
-		for (int j = 0; j < time.size(); j++)
-		{
-			if (processlist[i].starttime == time[i])
-			{
-				temp1 = true;
-				if (processlist[i].starttime + processlist[i].finishtime == time[j])
-					temp2 = true;
-}
-		}
-		if (temp1 == false)
-		{
-			time.push_back(processlist[i].starttime);
-		}
-		if (temp2 == false)
-			time.push_back(processlist[i].starttime + processlist[i].finishtime);
-	}
-	//sort the order using sort() function
-	sort(time.begin(), time.end());
+    for (int i = 0; i < processlist.size(); i++)
+    {
+        //temp1 = false;
+        //temp2 = false;
+        for (int j = 0; j < time.size(); j++)
+        {
+            if (processlist[i].starttime == time[i])
+            {
+                temp1 = true;
+                if (processlist[i].starttime + processlist[i].finishtime == time[j])
+                    temp2 = true;
+            }
+        }
+        if (temp1 == false)
+        {
+            time.push_back(processlist[i].starttime);
+        }
+        if (temp2 == false)
+            time.push_back(processlist[i].starttime + processlist[i].finishtime);
+    }
+    //sort the order using sort() function
+    sort(time.begin(), time.end());
 }
 
 void mapdisplay(vector<memoryblock> &memorymap, int memorysize, int pagesize)
 {
-	int num=0, start = 0, end = 0, processnumber = 0, pagenumber = 0;
-	cout << " Memory Map: " << endl;
+    int num=0, start = 0, end = 0, processnumber = 0, pagenumber = 0;
+    cout << " Memory Map: " << endl;
 
-	while (num < (memorysize / pagesize))
-	{
-		if (!memorymap[num].free)
-		{
-			if (start != 0)
-			{
-				end = num - 1;
-				cout << "   " << start * pagesize << "-" << ((end + 1)*pagesize) - 1 << ": Free frame(s)" << endl;
-				start = 0;
-			}
-			cout << "    " << memorymap[num].start << "-" << memorymap[num].end << " :Process " << memorymap[num].processnumber << " ,Page " << memorymap[num].pagenumber << endl;
-		}
-		else if (start = 0)
-			start = num;
-		num++;
-	}
-	if (start != 0)
-	{
-		end = num - 1;
-		cout << "    " << start * pagesize << "-" << ((end + 1)*pagesize) - 1 << ": Free frame(s)" << endl;
-	}
+    while (num < (memorysize / pagesize))
+    {
+        if (!memorymap[num].free)
+        {
+            if (start != 0)
+            {
+                end = num - 1;
+                cout << "   " << start * pagesize << "-" << ((end + 1)*pagesize) - 1 << ": Free frame(s)" << endl;
+                start = 0;
+            }
+            cout << "    " << memorymap[num].start << "-" << memorymap[num].end << " :Process " << memorymap[num].processnumber << " ,Page " << memorymap[num].pagenumber << endl;
+        }
+        else if (start = 0)
+            start = num;
+        num++;
+    }
+    if (start != 0)
+    {
+        end = num - 1;
+        cout << "    " << start * pagesize << "-" << ((end + 1)*pagesize) - 1 << ": Free frame(s)" << endl;
+    }
 
 }
+
+int check(vector<memoryblock> &memorymap, int memorysize)
+{
+    int space = 0;
+    for (int i = 0; i < memorymap.size(); i++)
+    {
+        if (memorymap[i].free)
+        {
+            space++;
+        }
+        else
+        {
+            space = 0;
+        }
+
+        if (space == memorysize)
+        {
+            return i + 1 - memorysize;
+        }
+    }
+    return -1;
+}
+
+
+bool addmemory(vector<memoryblock> &memorymap, int pagesize, process &input)
+{
+    int size;
+    int startblock = -1;
+    int numberofpage = 1;
+    double blocksize;
+    bool add=false;
+    int num = 0;
+    int space = -1;
+
+
+    //loop
+    for (int i = 0; i < input.numberofBlocks; i++)
+    {
+        blocksize = input.blockofsize[i] / pagesize;
+
+        for (int j = i; j < input.numberofBlocks; j++)
+        {
+            blocksize += (input.blockofsize[j] / pagesize);
+        }
+
+        size = (int)blocksize;
+        startblock = check(memorymap, blocksize);
+
+        if (startblock == -1)
+        {
+            size = (int)blocksize;
+            for (int i = 0; i < memorymap.size();i++)
+            {
+                if (memorymap[i].free)
+                    num++;
+                else
+                    num = 0;
+                if (num == size)
+                {
+                    space = i + 1 - blocksize;
+                }
+            }
+        }
+        if (startblock != -1)
+        {
+            add = true;
+            for (int j = startblock; j < (startblock + ((input.blockofsize[i] + 99) / pagesize)); j++)
+            {
+                memorymap[j].processnumber = input.number;
+                memorymap[j].pagenumber = numberofpage;
+                memorymap[j].free = false;
+                numberofpage++;
+            }
+        }
+
+    }
+
+    return add;
+}
+
 
 void out(vector<process> &processlist, vector<int> &time,int memorysize,int pagesize)
 {
-	int temp;
-	process temp2;
-	vector<memoryblock> memorymap;
-	vector<int> inqueue;
-	memoryblock line;
+    int temp;
+    process temp2;
+    vector<memoryblock> memorymap;
+    vector<int> inqueue;
+    memoryblock line;
 
 
-	//init memory map
-	for (int i = 0; i < (memorysize / pagesize); i++)
-	{
-		line.start = i * pagesize;
-		line.end = ((i + 1)*pagesize) - 1;
-		line.processnumber = -1;
-		line.pagenumber = -1;
-		line.free = true;
-		memorymap.push_back(line);
-	}
+    //init memory map
+    for (int i = 0; i < (memorysize / pagesize); i++)
+    {
+        line.start = i * pagesize;
+        line.end = ((i + 1)*pagesize) - 1;
+        line.processnumber = -1;
+        line.pagenumber = -1;
+        line.free = true;
+        memorymap.push_back(line);
+    }
 
-	//process loop
-	for (int i = 0; i < time.size(); i++)
-	{
-		//output time
-		cout << " t= " << time[i] << " : ";
+    //process loop
+    for (int i = 0; i < time.size(); i++)
+    {
+        //output time
+        cout << " t= " << time[i] << " : ";
 
-		//add new process
-		for (int j = 0; j < processlist.size(); j++)
-		{
-			if (processlist[j].starttime == time[i])
-			{
-				inqueue.push_back(processlist[j].number);
-				cout << "Process " << processlist[j].number << " arrive" << endl;
-				cout << " Input Queue: [";
-				for (int k = 0; k < inqueue.size(); k++)
-					cout << inqueue[k] << " ";
-				cout << "]" << endl;
-			}
-			else if (processlist[j].starttime + processlist[j].finishtime == time[i])
-			{
-				cout << "process " << processlist[j].number << " complete" << endl;
-				for (int i = 0; i < memorymap.size(); i++)
-				{
-					if (memorymap[i].processnumber == processlist[j].number)
-					{
-						memorymap[i].free = true;
-					}
-				}
-				mapdisplay(memorymap, memorysize, pagesize);
-			}
-		}
-		//add process to memory map
+        //add new process
+        for (int j = 0; j < processlist.size(); j++)
+        {
+            if (processlist[j].starttime == time[i])
+            {
+                inqueue.push_back(processlist[j].number);
+                cout << "Process " << processlist[j].number << " arrives" << endl;
+                cout << " Input Queue: [";
+                for (int k = 0; k < inqueue.size(); k++)
+                    cout << inqueue[k] << " ";
+                cout << "]" << endl;
+            }
+            else if (processlist[j].starttime + processlist[j].finishtime == time[i])
+            {
+                cout << "Process " << processlist[j].number << " completes" << endl;
+                for (int i = 0; i < memorymap.size(); i++)
+                {
+                    if (memorymap[i].processnumber == processlist[j].number)
+                    {
+                        memorymap[i].free = true;
+                    }
+                }
+                mapdisplay(memorymap, memorysize, pagesize);
+            }
+        }
+        //add process to memory map if there is enough frames
+        while (inqueue.size() != 0)
+        {
+            temp2 = processlist[inqueue.front() - 1];
+            if (addmemory(memorymap, pagesize, temp2)) {
 
-	}
+                cout << "    MM moves process " << temp2.number << " to memory" << endl;
+                inqueue.erase(inqueue.begin());
+                cout << "    Input Queue: [";
+                for (int k = 0; k < inqueue.size(); k++)
+                {
+                    cout << inqueue[k] << " ";
+                }
+                cout << "]" << endl;
+
+                mapdisplay(memorymap, memorysize, pagesize);
+
+            }
+
+        }
+
+    }
 }
-
-
 
 int main() {
 
-	int memomrySize = 0;
-	int pageSize = 0;
-	int numOfBlock;
+    int memomrySize = 0;
+    int pageSize = 0;
+    int numOfBlock;
 
-	cout << "Enter memory size (kb): " << endl;
-	cin >> memomrySize;
+    cout << "Enter memory size (kb): " << endl;
+    cin >> memomrySize;
 
-	cout << "Enter page size (1:100, 2:200, 3:300): " << endl;
-	cin >> pageSize;
-	if (pageSize == 1)
-	{
-		pageSize = 100;
-	}
-	else if (pageSize == 2)
-	{
-		pageSize = 200;
-	}
-	else if (pageSize == 3)
-	{
-		pageSize = 300;
-	}
+    cout << "Enter page size (1:100, 2:200, 3:300): " << endl;
+    cin >> pageSize;
+    if (pageSize == 1)
+    {
+        pageSize = 100;
+    }
+    else if (pageSize == 2)
+    {
+        pageSize = 200;
+    }
+    else if (pageSize == 3)
+    {
+        pageSize = 300;
+    }
 
-	vector<process> processlist;
-	vector<int> time;
+    vector<process> processlist;
+    vector<int> time;
 
-	string file = "in1.txt";
-	readfile(file, processlist);
-	processtime(processlist, time);
-	out(processlist, time, memomrySize, pageSize);
+    string file = "in1.txt";
+    readfile(file, processlist);
+    processtime(processlist, time);
+    out(processlist, time, memomrySize, pageSize);
 
-	return 0;
+    //calculate the average
+    int totalTurnAroundTime = 0;
+    double averageTurnAroundTime = 0;
+    for (int i = 0; i < processlist.size(); i++)
+    {
+        processlist[i].turnAroundTime = (processlist[i].finishtime
+                                         - processlist[i].starttime);
+        totalTurnAroundTime += processlist[i].turnAroundTime;
+    }
+    averageTurnAroundTime = double(totalTurnAroundTime)/processlist.size();
+    cout << "Average Turn Around Time: " << averageTurnAroundTime << " ("<< totalTurnAroundTime<<"/"<<processlist.size()<<")"<<endl;
+
+    system("PAUSE");
 }
